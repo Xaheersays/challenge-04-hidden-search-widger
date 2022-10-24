c3d64a29dfbd359ac3f5a0041274f9afee732749
unknown <shzaheer514@gmail.com>
2022-10-24 18:00:57 +0530

    code

---
+++ javascript/index.js
@@ -0,0 +1,11 @@
+document.addEventListener("DOMContentLoaded",function(){
+    //search btn
+    let btn = document.getElementById("search");
+    // input field
+    let inputField = document.getElementById("inputField")
+    // toggling class
+    btn.addEventListener("click",function(){
+        inputField.classList.toggle("hide-ip")
+
+    });
+});
